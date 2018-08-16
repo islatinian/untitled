937b3b39b76fc78687ec995744ccb27ddbc6e9f0
Iana <isla.tinian@gmail.com>
2018-08-16 14:46:31 +0300

    asdasdasd

--- src/ghjk.java
+++ src/ghjk.java
@@ -1,5 +1,7 @@
 public class ghjk {
     public static void main(String[] args) {
-        System.out.println("sdfghjk");
+        System.out.println("sdfghghjjk");
+        System.out.println("sdfghsfsfasfafafghjjk");
+        System.out.println("sdasasdasdasdfghghjjk");
     }
 }
